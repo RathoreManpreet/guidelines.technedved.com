560a1a6507c4668e792d965d93bcd5f9071afcb5
Manpreet <manpreet@kmrinfotech.com>
2019-05-19 13:11:44 +0530

    frontend page design ready

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -27,14 +27,12 @@
                     Coding Style
                 </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Laravel &amp; PHP</a>
+                    <a class="dropdown-item" href="#">Laravel &amp;amp; PHP</a>
                     <div class="dropdown-divider"></div>
                     <a class="dropdown-item" href="#">Css</a>
-                    <a class="dropdown-item" href="#">Javascript &amp; Vue</a>
+                    <a class="dropdown-item" href="#">Javascript &amp;amp; Vue</a>
                 </div>
             </li>
-
-
         </ul>
     </div>
 </nav>
